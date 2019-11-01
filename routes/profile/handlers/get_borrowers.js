a5d256a6b1552d267cefb27fa69fb921ba81d889
atoyebi92 <itisdeji@gmail.com>
2019-11-02 00:49:47 +0100

    env.json

---
+++ routes/profile/handlers/get_borrowers.js
@@ -0,0 +1,23 @@
+var utils = require('mlar')('mt1l');
+const routemeta = require('mlar')('routemeta');
+const auth_middleware = require('mlar')('authmiddleware');
+const service = require('mlar').mreq('services', 'profile/get_borrowers');
+
+function vinfo(req, res, next){ 
+        const data = {...req.body, ...req.query, ...req.headers, ...req.params};
+        
+        service(data)
+        .then(response => {
+            utils.jsonS(res, response, "Profile"); 
+        })
+        .catch(error => {
+            utils.jsonF(res, null, error.message); 
+        })
+}
+
+vinfo.routeConfig = {};
+vinfo.routeConfig.path = "/:user_id/list"; 
+vinfo.routeConfig.method = "get"; 
+vinfo.routeConfig.middlewares = [auth_middleware, routemeta('get_profiles', 'none')];
+module.exports = vinfo;
+
