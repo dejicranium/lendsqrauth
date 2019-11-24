4e68d0902f30d8da32290d696349ad9a96b48db5
atoyebi92 <itisdeji@gmail.com>
2019-11-24 21:49:56 +0100

    cherrypick changes from develop commit

--- services/product/create.js
+++ services/product/create.js
@@ -30,12 +30,53 @@ function service(data){
 	q.fcall( async () => {
 		const validParameters = morx.validate(data, spec, {throw_error : true});
 		const params = validParameters.params;
+		
+		if (params.max_tenor) assert.digitsOnly(params.max_tenor, null, 'max tenor')
+		if (params.min_tenor) assert.digitsOnly(params.min_tenor, null, 'min tenor')
+		if (params.interest) {
+			try {
+				parseDouble(params.interest)
+			}
+			catch(e) {
+				throw new Error ("Interest should be an integer or double");
+			}
+		}
+
+		if (params.min_loan_amount) {
+			assert.digitsOnly(params.min_loan_amount) 
+			params.min_loan_amount = parseFloat(params.min_loan_amount).toFixed(2);
+
+		}
+		if (params.max_loan_amount) {
+			assert.digitsOnly(params.max_loan_amount) 
+			params.max_loan_amount = parseFloat(params.max_loan_amount).toFixed(2);
+
+		}
+
+
 		params.repayment_method = params.repayment_method.toLowerCase();
+		params.repayment_model = params.repayment_model.toLowerCase()
+		params.interest_period = params.interest_period.toLowerCase();
+		params.tenor_type = params.tenor_type.toLowerCase();
+
 
 		if (!['card', 'direct debit', 'bank transfer', 'cheque', 'cash'].includes(params.repayment_method))
 			throw new Error("Repayment method can only be card, direct debit, bank transfer, cheque or cash")
+<<<<<<< HEAD
 		if (params.product_name.length > 255) throw new Error("Product name cannot be more than 255 characters");
 
+=======
+		
+		if (!['equal installments', 'reducing balance'].includes(params.repayment_model))
+			throw new Error('Repayment model can be either `equal installments` or `reducing balance`')
+
+		if (!['per day', 'per month', 'per annum'].includes(params.interest_period))
+			throw new Error('Interest period should be one of `per day`, `per month` or `per annum`')
+		
+		
+			if (params.product_name.length > 255) throw new Error("Product name cannot be more than 255 characters");
+		
+>>>>>>> 7019bc4... verify loan product params
         let getProductName = models.product.findOne({
             where: {
                 profile_id: params.profile_id,
