82cdcf40a3db61f0754fde297bf81249c9b0394e
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2019-12-09 00:52:31 +0100

    only business and individual lenders should see members

--- routes/profile/handlers/get_team_members.js
+++ routes/profile/handlers/get_team_members.js
@@ -3,28 +3,35 @@ const routemeta = require('mlar')('routemeta');
 const auth_middleware = require('mlar')('authmiddleware');
 const service = require('mlar').mreq('services', 'profile/get_team_members');
 const profile_middleware = require('mlar')('profileVerifyMiddleware');
+const has_role = require('mlar')('hasRoleMiddleware');
 
-function vinfo(req, res, next){ 
-        const data = {...req.body, ...req.query, ...req.headers, ...req.params};
-        
-        // pass profile 
-        data.user = req.user;
-        data.profile = req.profile;
-        service(data)
+function vinfo(req, res, next) {
+    const data = {
+        ...req.body,
+        ...req.query,
+        ...req.headers,
+        ...req.params
+    };
+
+    // pass profile 
+    data.user = req.user;
+    data.profile = req.profile;
+    service(data)
         .then(response => {
-            utils.jsonS(res, response, "Profile"); 
+            utils.jsonS(res, response, "Profile");
         })
         .catch(error => {
-            utils.jsonF(res, null, error.message); 
+            utils.jsonF(res, null, error.message);
         })
 }
 
 vinfo.routeConfig = {};
-vinfo.routeConfig.path = "/team"; 
-vinfo.routeConfig.method = "get"; 
+vinfo.routeConfig.path = "/team";
+vinfo.routeConfig.method = "get";
 vinfo.routeConfig.middlewares = [
-    auth_middleware, 
+    auth_middleware,
     profile_middleware,
-    routemeta('get_team_members', 'none')];
+    has_role(['business_lender', 'invidivual_lender']),
+    routemeta('get_team_members', 'none')
+];
 module.exports = vinfo;
-
