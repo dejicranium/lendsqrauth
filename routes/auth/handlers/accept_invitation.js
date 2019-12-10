ac19412ae20b609d682f8178c422151f6b2cf3da
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2019-12-10 15:42:09 +0100

    stop the 503

---
+++ routes/auth/handlers/accept_invitation.js
@@ -0,0 +1,29 @@
+var utils = require('mlar')('mt1l');
+const service = require('mlar').mreq('services', 'auth/reject_invitation');
+const routemeta = require('mlar')('routemeta');
+const auth_middleware = require('mlar')('authmiddleware');
+const has_perm_middleware = require('mlar')('hasPermMiddleware');
+
+function vinfo(req, res, next) {
+    const data = {
+        ...req.query,
+        ...req.body,
+        ...req.headers
+    };
+
+    service(data)
+        .then(response => {
+            utils.jsonS(res, response, "Process completed successfully");
+        })
+        .catch(error => {
+            utils.jsonF(res, null, error.message);
+        })
+}
+
+vinfo.routeConfig = {};
+vinfo.routeConfig.path = "/invites/accept";
+vinfo.routeConfig.method = "put";
+vinfo.routeConfig.middlewares = [
+    routemeta('accept_invitation', 'none')
+];
+module.exports = vinfo;
