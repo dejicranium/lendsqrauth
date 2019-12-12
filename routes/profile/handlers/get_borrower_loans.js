899e29a080d19fb6c4377d8cfec453b3aa37882f
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2019-12-12 16:31:24 +0100

    added get borrower loans

---
+++ routes/profile/handlers/get_borrower_loans.js
@@ -0,0 +1,36 @@
+var utils = require('mlar')('mt1l');
+const routemeta = require('mlar')('routemeta');
+const auth_middleware = require('mlar')('authmiddleware');
+const service = require('mlar').mreq('services', 'profile/get_borrowers_loans');
+const profile_middleware = require('mlar')('profileVerifyMiddleware');
+const has_role = require('mlar')('hasRoleMiddleware');
+
+
+function vinfo(req, res, next) {
+    const data = {
+        ...req.body,
+        ...req.query,
+        ...req.headers,
+        ...req.params
+    };
+    data.user = req.user;
+    data.profile = req.profile;
+    service(data)
+        .then(response => {
+            utils.jsonS(res, response, "Loans");
+        })
+        .catch(error => {
+            utils.jsonF(res, null, error.message);
+        })
+}
+
+vinfo.routeConfig = {};
+vinfo.routeConfig.path = "/:profile_id/loans";
+vinfo.routeConfig.method = "get";
+vinfo.routeConfig.middlewares = [
+    auth_middleware,
+    profile_middleware,
+    has_role(['admin', 'borrower']),
+    routemeta('get_borrower_loans', 'none')
+];
+module.exports = vinfo;
---
+++ services/profile/get_borrowers_loans.js
@@ -0,0 +1,60 @@
+const models = require('mlar')('models');
+const ErrorLogger = require('mlar')('errorlogger');
+const morx = require('morx');
+const q = require('q');
+const validators = require('mlar')('validators');
+const assert = require('mlar')('assertions');
+const paginate = require('mlar')('paginate');
+
+/**
+ * This endpoint gets a list of lenders
+ * But the kindof lenders that are returned depends on the parameters passed
+ * 
+ */
+var spec = morx.spec({})
+    .build('profile_id', 'required:true, eg:1')
+    .build('profile_type', 'required:false, eg:1')
+    .end();
+
+function service(data) {
+
+    var d = q.defer();
+    const page = data.page ? Number(data.page) : 1;
+    const limit = data.limit ? Number(data.limit) : 20;
+    const offset = page ? (page - 1) * limit : false;
+
+    data.limit = limit;
+    data.offset = offset;
+
+    q.fcall(async () => {
+            const validParameters = morx.validate(data, spec, {
+                throw_error: true
+            });
+            const params = validParameters.params;
+
+            data.where = {
+                borrower_id: data.profile_id
+            }
+            data.include = [{
+                model: models.product
+            }]
+
+            return models.collection.findAndCountAll(data);
+
+
+        })
+        .then((collections) => {
+
+            if (!collections) throw new Error("No profiles");
+            d.resolve(paginate(collections.rows, 'collections', collections.count, limit, page));
+
+        })
+        .catch((err) => {
+            d.reject(err);
+        });
+
+    return d.promise;
+
+}
+service.morxspc = spec;
+module.exports = service;
