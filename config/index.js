c3cbfeea01350104a75f5e2e7dc844d863e5394f
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-01-14 09:55:54 +0100

    add base url

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     sender_email: process.env.sender_email || 'support@lendsqr.com',
     notif_base_url: process.env.notif_base_url || 'https://v2-test.lendsqr.com/api/v1/notification/',
     utility_base_url: process.env.utility_base_url || 'https://v2-test.lendsqr.com/api/v1/util/',
-    base_url: process.env.base_url || 'https://v2-test.lendsqr.com/',
+    base_url: process.env.base_url || 'http://staging-lb-978527258.us-east-2.elb.amazonaws.com/api/v1/',
     admin_reg_token: process.env.admin_reg_token || 'saraytheiconoclast%32$6',
 
     aws_region: process.env.aws_region || 'us-east-2',
@@ -26,4 +26,4 @@ module.exports = {
     wallet_service_base_url: process.env.wallet_service_base_url || 'https://wallet-service-lendsqr.herokuapp.com/api/v1/',
     sqs_jobs_queue_url: process.env.sqs_jobs_queue_url || 'https://sqs.us-east-2.amazonaws.com/350152003452/LendsqrAsyncJobs'
 
-}
+};
