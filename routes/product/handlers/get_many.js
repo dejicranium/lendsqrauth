fd719208347aaae16c8c4c6248a025023ff0f578
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-01-15 18:57:12 +0100

    has_role to get products

--- routes/product/handlers/get_many.js
+++ routes/product/handlers/get_many.js
@@ -3,25 +3,32 @@ const routemeta = require('mlar')('routemeta');
 const auth_middleware = require('mlar')('authmiddleware');
 const service = require('mlar').mreq('services', 'product/get');
 const profile_middleware = require('mlar')('profileVerifyMiddleware');
+const has_role = require('mlar')('hasRoleMiddleware');
 
-function vinfo(req, res, next){ 
-        const data = {...req.body, ...req.query, ...req.headers, ...req.params};
-        data.profile = req.profile;
-        service(data)
+function vinfo(req, res, next) {
+    const data = {
+        ...req.body,
+        ...req.query,
+        ...req.headers,
+        ...req.params
+    };
+    data.profile = req.profile;
+    service(data)
         .then(response => {
-            utils.jsonS(res, response, "Products"); 
+            utils.jsonS(res, response, "Products");
         })
         .catch(error => {
-            utils.jsonF(res, null, error.message); 
+            utils.jsonF(res, null, error.message);
         })
 }
 
 vinfo.routeConfig = {};
-vinfo.routeConfig.path = "/"; 
-vinfo.routeConfig.method = "get"; 
+vinfo.routeConfig.path = "/";
+vinfo.routeConfig.method = "get";
 vinfo.routeConfig.middlewares = [
-    auth_middleware, 
+    auth_middleware,
     profile_middleware,
-    routemeta('get_products', 'none')];
+    has_role(['admin', 'individual_lender', 'business_lender']),
+    routemeta('get_products', 'none')
+];
 module.exports = vinfo;
-
