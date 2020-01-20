67298194166db018b8d679a1cc758731b49d6606
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-01-20 01:36:14 +0100

    can't delete an active collection

--- services/collection/delete.js
+++ services/collection/delete.js
@@ -1,59 +1,63 @@
 const models = require('mlar')('models');
-const ErrorLogger = require('mlar')('errorlogger'); 
-const morx = require('morx'); 
-const q = require('q'); 
-const validators = require('mlar')('validators'); 
+const ErrorLogger = require('mlar')('errorlogger');
+const morx = require('morx');
+const q = require('q');
+const validators = require('mlar')('validators');
 const assert = require('mlar')('assertions');
 const AuditLog = require('mlar')('audit_log');
 
-var spec = morx.spec({}) 
-			   .build('collection_id', 'required:true, eg:1')   
-			   .end();
+var spec = morx.spec({})
+    .build('collection_id', 'required:true, eg:1')
+    .end();
 
-function service(data){
+function service(data) {
 
-	var d = q.defer();
-	const globalUserId = data.user.id || 1
-	q.fcall( async () => {
-		const validParameters = morx.validate(data, spec, {throw_error : true});
-		const params = validParameters.params;
-        return [models.collection.findOne({
-            where: {
-                id: params.collection_id
-            },
-            include: [
-                {
+    var d = q.defer();
+    const globalUserId = data.user.id || 1
+    q.fcall(async () => {
+            const validParameters = morx.validate(data, spec, {
+                throw_error: true
+            });
+            const params = validParameters.params;
+            return [models.collection.findOne({
+                where: {
+                    id: params.collection_id
+                },
+                include: [{
                     model: models.product,
                     required: false
-                }
-            ]
-        }), params]
-        
-	}) 
-	.spread((collection, params) => { 
-        if (!collection) throw new Error("No collection found");
-        if(data.profile.id !== collection.lender_id) throw new Error("You can't delete a collection that you didn't create");
-        params.deleted_flag = 1;
-        params.deleted_on = new Date();
-        params.deleted_by = globalUserId;
+                }]
+            }), params]
 
-        return collection.update({...params})
-    }).then(async (collection)=>{
-        if (!collection) throw new Error("An error occured while deleting collection");
+        })
+        .spread((collection, params) => {
+            if (!collection) throw new Error("No collection found");
+            if (data.profile.id !== collection.lender_id) throw new Error("You can't delete a collection that you didn't create");
+            if (collection.status === 'active') throw new Error("You cannot delete an active collection");
 
-        //audit log
-        let audit = new AuditLog(data.reqData, "DELETE", "Deleted collection " + collection.id);
-        await  audit.create();
+            params.deleted_flag = 1;
+            params.deleted_on = new Date();
+            params.deleted_by = globalUserId;
 
-        d.resolve("Successfully deleted collection");
-    })
-	.catch( (err) => {
+            return collection.update({
+                ...params
+            })
+        }).then(async (collection) => {
+            if (!collection) throw new Error("An error occured while deleting collection");
 
-		d.reject(err);
+            //audit log
+            let audit = new AuditLog(data.reqData, "DELETE", "Deleted collection " + collection.id);
+            await audit.create();
 
-	});
+            d.resolve("Successfully deleted collection");
+        })
+        .catch((err) => {
 
-	return d.promise;
+            d.reject(err);
+
+        });
+
+    return d.promise;
 
 }
 service.morxspc = spec;
