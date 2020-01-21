39570db3b468a86a7c5868759ce7bb552abb3d17
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-01-21 01:32:39 +0100

    fix interest contains digits

--- utils/validators.js
+++ utils/validators.js
@@ -4,7 +4,7 @@ module.exports = {
     },
 
     isDigitsOrDecimal(entity) {
-        return /^[1-9]\d*(\.\d+)?$/.test(entity)
+        return /^[0-9]\d*(\.\d+)?$/.test(entity)
     },
 
     isOfLength(entity, length) {
@@ -14,26 +14,25 @@ module.exports = {
     },
 
     isNuban(accountNumber) {
-        const self = this; 
+        const self = this;
         return self.isDigits(accountNumber) && self.isOfLength(accountNumber, 10);
     },
 
     isDate(date) {
         date = new Date(date)
         date = date.toString().toLowerCase();
-        
-        return date != 'invalid date';  
-        
+
+        return date != 'invalid date';
+
     },
     isEmail(string) {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(string).toLowerCase());
-        
+
     },
     areMutuallyExclusive(values) {
         // if original length of values is not the same as the size of the set, it means some values are not unique
         return values.length == new Set(values).size;
-        
+
     },
 }
-
