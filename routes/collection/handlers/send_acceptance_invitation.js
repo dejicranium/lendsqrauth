f3b96c56dfe49df2e9a5205da0dd3c82b38e6b83
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-01-26 01:17:24 +0100

    fix

--- routes/collection/handlers/send_acceptance_invitation.js
+++ routes/collection/handlers/send_acceptance_invitation.js
@@ -1,7 +1,7 @@
 var utils = require('mlar')('mt1l');
 const routemeta = require('mlar')('routemeta');
 const auth_middleware = require('mlar')('authmiddleware');
-const service = require('mlar').mreq('services', 'collection/send_acceptance_invitation');
+const service = require('mlar').mreq('services', 'collection/send_acceptance_notification');
 const profile_middleware = require('mlar')('profileVerifyMiddleware');
 
 function vinfo(req, res, next) {
