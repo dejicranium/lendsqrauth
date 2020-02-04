dbf5bd090c1b5907cc0f164501bbaa72b41284a7
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-02-04 22:47:51 +0100

    if user is deleted , resend invitation

--- services/auth/inviteuser.js
+++ services/auth/inviteuser.js
@@ -71,7 +71,7 @@ function service(data) {
 			];
 		})
 		.spread(async (invitation, user, params) => {
-			if (invitation && invitation.status !== 'declined') {
+			if (invitation && invitation.status !== 'declined' && !invitation.status.deleted_flag) {
 				throw new Error('Invitation has been sent already');
 			}
 
