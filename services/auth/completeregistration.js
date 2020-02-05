f0b8bc308c776ba3e7c17c49035ab0910fea7437
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-02-05 18:56:49 +0100

    activate profile

--- services/auth/completeregistration.js
+++ services/auth/completeregistration.js
@@ -117,7 +117,7 @@ function service(data) {
             if (accepted_user_profile && accepted_user_profile.id) {
                 // add a parent_profile when it's accepted
                 accepted_user_profile.parent_profile_id = invite.inviter;
-                accepted_user_profile.status = 'pending';
+                accepted_user_profile.status = 'active';
                 await accepted_user_profile.save()
             }
 
