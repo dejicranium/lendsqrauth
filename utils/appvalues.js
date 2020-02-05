bffdb299f96638bc0403c1e090acd8b69e305ff9
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-02-05 15:04:02 +0100

    fix users

--- utils/appvalues.js
+++ utils/appvalues.js
@@ -1,10 +1,9 @@
 module.exports = {
     DEFAULT_EXCLUDES: [
-        'created_on', 
-        'modified_on', 
-        'created_by', 
-        'modified_by', 
-        'deleted_by', 
+        'modified_on',
+        'created_by',
+        'modified_by',
+        'deleted_by',
         'deleted_on',
         'password',
         'deleted_flag'
