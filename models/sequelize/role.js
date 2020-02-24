aa2423405ae04557ab9c5183e135e6929f565088
chigozie3911 <chigozie@lendsqr.com>
2020-02-24 14:52:56 +0100

    added created_on to roles

--- models/sequelize/role.js
+++ models/sequelize/role.js
@@ -1,18 +1,23 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const role = sequelize.define('role', {
-    name: DataTypes.STRING,
-    deleted_flag: DataTypes.BOOLEAN,
-    created_by: DataTypes.INTEGER,
-    modified_on: DataTypes.DATE,
-    modified_by: DataTypes.DATE,
-    deleted_on: DataTypes.DATE,
-    deleted_by: DataTypes.INTEGER,
-  }, {
-    timestamps: false
-  });
+  const role = sequelize.define(
+    'role',
+    {
+      name: DataTypes.STRING,
+      deleted_flag: DataTypes.BOOLEAN,
+      created_on: DataTypes.DATE,
+      created_by: DataTypes.INTEGER,
+      modified_on: DataTypes.DATE,
+      modified_by: DataTypes.DATE,
+      deleted_on: DataTypes.DATE,
+      deleted_by: DataTypes.INTEGER
+    },
+    {
+      timestamps: false
+    }
+  );
   role.associate = function(models) {
-    role.hasMany(models.profile, {foreignKey: 'role_id'});
+    role.hasMany(models.profile, { foreignKey: 'role_id' });
   };
   return role;
 };
