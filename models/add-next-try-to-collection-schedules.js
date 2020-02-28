ff722b8e33fd4726b2227e6cf01baea9809725b3
Deji Atoyebi <deji.atoyebi@flutterwavego.com>
2020-02-29 00:20:17 +0100

    new column to collection schedules

---
+++ models/add-next-try-to-collection-schedules.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const add - next -
+    try -to - collection - schedules = sequelize.define('add-next-try-to-collection-schedules', {
+      id: DataTypes.INTEGER
+    }, {});
+  add - next -
+    try -to - collection - schedules.associate = function(models) {
+      // associations can be defined here
+    };
+  return add - next -
+    try -to - collection - schedules;
+};
