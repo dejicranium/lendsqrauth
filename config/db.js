1ec67128573e68489c160e012e2948af5eb524a3
Chigozie Madubuko <chigoziemadubuko@gmail.cpm>
2020-03-02 11:34:29 +0100

    added env setup for prod environment

--- config/db.js
+++ config/db.js
@@ -7,14 +7,13 @@ module.exports = {
     host: process.env.DEVHOST || 'staging.cyock39x6hx4.us-east-2.rds.amazonaws.com',
     dialect: 'mysql'
   },
-  production: {
-    username: process.env.PRODUN,
-    password: process.env.PRODPW,
-    database: process.env.PRODDB,
-    host: process.env.PRODHOST,
+  live: {
+    username: 'lender',
+    password: 'Su94LtyGwDx2aV7c',
+    database: 'lendsqr_auth',
+    host: 'staging.cyock39x6hx4.us-east-2.rds.amazonaws.com',
     dialect: 'mysql'
   },
-
   staging: {
     username: 'lender',
     password: 'Su94LtyGwDx2aV7c',
