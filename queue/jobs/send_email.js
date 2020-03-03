6d761f398c446a8a5ca5a6af7fed57d9fb1af515
Chigozie Madubuko <chigoziemadubuko@gmail.cpm>
2020-03-03 08:39:24 +0100

    added live queue

--- queue/jobs/send_email.js
+++ queue/jobs/send_email.js
@@ -2,36 +2,34 @@ const send_message_to_queue = require('mlar').mreq('queue', 'send_message');
 const config = require('../../config');
 
 async function sendEmail(context_id, recipient, payload) {
-    let action_type = "send_email";
-    context_id = context_id.toString();
+  let action_type = 'send_email';
+  context_id = context_id.toString();
 
-    let messageData = {
-        MessageAttributes: {
-            "context_id": {
-                DataType: "String",
-                StringValue: context_id
-            },
-            "recipient": {
-                DataType: "String",
-                StringValue: recipient
-            },
-            "sender": {
-                DataType: "String",
-                StringValue: config.sender_email,
-            },
-            "action_type": {
-                DataType: "String",
-                StringValue:"send_email",
-            }
-        },
-        MessageBody: JSON.stringify({context_id, recipient, payload, action_type, sender: config.sender_email}),
-        //MessageGroupId: "CollectionsDue",
-        QueueUrl: config.sqs_jobs_queue_url,
-    };
+  let messageData = {
+    MessageAttributes: {
+      context_id: {
+        DataType: 'String',
+        StringValue: context_id
+      },
+      recipient: {
+        DataType: 'String',
+        StringValue: recipient
+      },
+      sender: {
+        DataType: 'String',
+        StringValue: process.env.sender_email
+      },
+      action_type: {
+        DataType: 'String',
+        StringValue: 'send_email'
+      }
+    },
+    MessageBody: JSON.stringify({ context_id, recipient, payload, action_type, sender: config.sender_email }),
+    //MessageGroupId: "CollectionsDue",
+    QueueUrl: process.env.sqs_jobs_queue_url
+  };
 
-    return await send_message_to_queue(messageData);
-
+  return await send_message_to_queue(messageData);
 }
-
 
 module.exports = sendEmail;
